530a8f641ae7c0be676ec51439fb450df0a5984e
Peter Mathis <peter.mathis@kombinat.at>
2020-04-21 16:50:00 +0200

    implement new layer to enable plone.app.z3cform overrides

--- plonetheme/barceloneta/interfaces.py
+++ plonetheme/barceloneta/interfaces.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+from plone.app.z3cform.interfaces import IPloneFormLayer
 from plone.theme.interfaces import IDefaultPloneLayer
 
 
 class IBarcelonetaLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer and a plone skin
        marker.
+    """
+
+
+class IBarcelonetaFormLayer(IPloneFormLayer):
+    """Marker for plone.app.z3cform overrides
     """
